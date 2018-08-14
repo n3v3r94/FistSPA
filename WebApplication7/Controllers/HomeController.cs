a2839996ad3ac85eea0ea764cd40615c5fb20801
n3v3r94 <ivanovskihristo34@gmail.com>
2018-08-14 15:14:07 +0300

    Update Project

---
+++ WebApplication7/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication7.Data;
+using WebApplication7.Data.Models;
+using WebApplication7.Models;
+
+namespace WebApplication7.Controllers
+{
+  
+    public class HomeController : Controller
+    {
+
+        private readonly ApplicationDbContext db;
+
+    
+        public HomeController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       public JsonResult GetAllAppointment()
+        {
+
+
+            return new JsonResult(this.db.Events.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult AddEvent([FromBody] Event app)
+        {
+            this.db.Events.Add(app);
+            db.SaveChanges();
+
+            return new JsonResult("Success");
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication7/Data/Models/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication7.Data.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+
+        public string start { get; set; }
+
+        public string end { get; set; }
+    }
+}
